aa48a9a2990474b6bd74a031a4b08ce48eede703
JaylanEsquivel <jaylan1997@hotmail.com>
2023-01-29 01:38:03 -0300

    Atualizando create

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -23,7 +23,7 @@ namespace ModuloApi.Controllers
 
             _context.Add(contato);
             _context.SaveChanges();
-            return Ok(contato);
+            return CreatedAtAction(nameof(ObterPorId), new { id = contato.Id }, contato);
 
         }
 
